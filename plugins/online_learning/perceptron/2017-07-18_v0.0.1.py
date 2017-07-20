dff8ee766e9a2289e2fb9dbb26bd4e581328d59b
Yuefeng Li <yuefengli2016@gmail.com>
2017-07-20 20:57:16 +0100

    moved submodule to api_dev branch

--- plugins/online_learning/perceptron/2017-07-18_v0.0.1.py
+++ plugins/online_learning/perceptron/2017-07-18_v0.0.1.py
@@ -1,8 +1,26 @@
 from hyperstream import Tool, StreamInstance
+from random import choice
+from numpy import array, dot, random
+
+unit_step = lambda x: 0 if x < 0 else 1
+
+training_data = [
+    (array([0,0,1]), 0),
+    (array([0,1,1]), 1),
+    (array([1,0,1]), 1),
+    (array([1,1,1]), 1),
+]
+
+w = random.rand(3)
+learning_rate = 0.2
 
 
 def perceptron_update(x, w):
-    y = 0
+    x, expected = choice(training_data)
+    result = dot(w,x)
+    error = expected - unit_step(result)
+    w = w + learning_rate * error * x
+    y = unit_step(result)
     return w, y
 
 
